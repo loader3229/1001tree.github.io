f6c80d6c253ff0ca9044707e1ca7f75a25f7a8e9
user incremental <userincre@qq.com>
2025-09-15 23:06:37 +0800

    v0.39.9998

--- js/world/104.js
+++ js/world/104.js
@@ -20,7 +20,11 @@ addLayer("104", {
             ob2: false,
             ob3: false,
             ob4: false,
+            ob5: false,
+            bl: false,
+            ch: false,
             t5: false,
+            t6: false,
         }
     },
     layerShown() { return getGridData('main', this.layer) && (!options.hideWorld || !player.world[this.layer]) },
@@ -28,7 +32,10 @@ addLayer("104", {
         Normal: {
             content: [
                 ["display-text", function () {
-                    return `你所获得的最高点数是 <h2 class = 'nmpt'>${formatWhole(player['104'].ig ? Decimal.pow(2, player['104'].points.div(2)) : player['104'].points)}</h2>, 到达2048完成世界!`
+                    return `你所获得的最高点数是 <h2 class = 'nmpt'>${formatWhole(player['104'].ig ? Decimal.pow(2, player['104'].points.div(2)) : player['104'].points)}</h2>, 到达1024完成世界!`
+                }],
+                ["display-text", function () {
+                    return `W,A,S,D移动方块, L快速重开`
                 }],
                 ["display-text", function () {
                     return (player['104'].trig) ? `蓝色的规则可随时启用或关闭, 红色的规则只能在游戏开始之前调整!` : `完成世界来解锁其他规则!`
@@ -50,7 +57,7 @@ addLayer("104", {
                     ["clickable", 23],
                 ]
                 ],
-                ["clickables", [3, 4]]
+                ["clickables", [3, 4, 5]]
             ]
         }
     },
@@ -59,9 +66,10 @@ addLayer("104", {
         { key: "a", description: "", onPress() { layers['104'].doMovement(3) } },
         { key: "s", description: "", onPress() { layers['104'].doMovement(2) } },
         { key: "d", description: "", onPress() { layers['104'].doMovement(4) } },
+        { key: "l", description: "", onPress() { player['104'].losetrig = true } },
     ],
     update(diff) {
-        if (player['104'].points.gte(2048) && (!player['104'].trig)) {
+        if (player['104'].points.gte(1024) && (!player['104'].trig)) {
             completeWorld('104')
             player['104'].trig = true
         }
@@ -79,17 +87,36 @@ addLayer("104", {
                 layers['104'].numGen()
             }
         }
+        if (player['104'].ch){
+            let r = chooseWeightInArray[[1,400],[0,96]]
+            if(r) layers['104'].switchGrid()
+        }
         player['104'].lastmove = fx
+    },
+    switchGrid(){
+        if(!player['104'].ch) return
     },
     canMerge(fx) {
         for (i in player['104'].grid) {
             let x = Math.floor(i / 100)
             let y = Math.floor(i % 100)
-            if (fx == 1) x--
-            if (fx == 2) x++
-            if (fx == 3) y--
-            if (fx == 4) y++
-            if (x <= 0 || y <= 0 || x >= 5 || y >= 5) continue
+            if (fx == 1){
+                x--
+                if(player['104'].bl) x--
+            }
+            if (fx == 2){
+                x++
+                if(player['104'].bl) x++
+            }
+            if (fx == 3){
+                y--
+                if(player['104'].bl) y--
+            }
+            if (fx == 4){
+                y++
+                if(player['104'].bl) y++
+            }
+            if (x <= 0 || y <= 0 || x >= (5+player['104'].t5+player['104'].t6) || y >= (5+player['104'].t5+player['104'].t6)) continue
             let z = x * 100 + y
             t1 = player['104'].grid[z]
             t2 = player['104'].grid[i]
@@ -106,7 +133,7 @@ addLayer("104", {
         return false
     },
     numGen() {
-        if (player['104'].cnt == (Math.pow(4 + player['104'].t5, 2)) && (!layers['104'].canMerge(1)) && (!layers['104'].canMerge(2)) && (!layers['104'].canMerge(3)) && (!layers['104'].canMerge(4))) {
+        if (player['104'].cnt == (Math.pow(4 + player['104'].t5 + player['104'].t6, 2)) && (!layers['104'].canMerge(1)) && (!layers['104'].canMerge(2)) && (!layers['104'].canMerge(3)) && (!layers['104'].canMerge(4))) {
             if (!player['104'].ud) {
                 player['104'].losetrig = true
                 return
@@ -121,15 +148,12 @@ addLayer("104", {
         let a = []
         for (i in player[this.layer].grid) {
             if (player[this.layer].grid[i].eq(0)) {
-                if (player['104'].t5) a.push(i)
-                else {
-                    if (((i % 100) < 5) && (Math.floor(i / 100) < 5)) a.push(i)
-                }
+                if (((i % 100) < (5+player['104'].t5+player['104'].t6)) && (Math.floor(i / 100) < (5+player['104'].t5+player['104'].t6))) a.push(i)
             }
         }
         let r = chooseOneInArray(a)
         let s = [[_D2, 80], [_D4, 15], [_D8, 5]]
-        p = player[this.layer].maxx
+        p = player['104'].maxx
         let s1 = [[p.div(1024), 75], [p.div(512), 12.5], [p.div(256), 1 / 16], [p.div(128), 1 / 32], [p.div(64), 1 / 64], [p.div(32), 1 / 128], [p.div(16), 1 / 128]]
         let sp1 = chooseWeightInArray([[_D(-6), 4], [_D0, 96]])
         let sp2 = chooseWeightInArray([[_D(-7), 4], [_D0, 96]])
@@ -161,42 +185,67 @@ addLayer("104", {
         let a = []
         if (fx == 1) {
             a = [101, 102, 103, 104, 201, 202, 203, 204, 301, 302, 303, 304, 401, 402, 403, 404]
-            if (player['104'].t5) {
+            if (player['104'].t5 || player['104'].t6) {
                 a = [101, 102, 103, 104, 105, 201, 202, 203, 204, 205, 301, 302, 303, 304, 305, 401, 402, 403, 404, 405, 501, 502, 503, 504, 505]
+            }
+            if (player['104'].t5 && player['104'].t6) {
+                a = [101, 102, 103, 104, 105, 106, 201, 202, 203, 204, 205, 206, 301, 302, 303, 304, 305, 306, 401, 402, 403, 404, 405, 406, 501, 502, 503, 504, 505, 506, 601, 602, 603, 604, 605, 606]
             }
         }
         if (fx == 2) {
             a = [401, 402, 403, 404, 301, 302, 303, 304, 201, 202, 203, 204, 101, 102, 103, 104]
-            if (player['104'].t5) {
+            if (player['104'].t5 || player['104'].t6) {
                 a = [505, 504, 503, 502, 501, 401, 402, 403, 404, 405, 301, 302, 303, 304, 305, 201, 202, 203, 204, 205, 101, 102, 103, 104, 105]
+            }
+            if (player['104'].t5 && player['104'].t6) {
+                a = [601, 602, 603, 604, 605, 606, 501, 502, 503, 504, 505, 506, 401, 402, 403, 404, 405, 406, 301, 302, 303, 304, 305, 306, 201, 202, 203, 204, 205, 206, 101, 102, 103, 104, 105, 106]
             }
         }
         if (fx == 3) {
             a = [101, 201, 301, 401, 102, 202, 302, 402, 103, 203, 303, 403, 104, 204, 304, 404]
-            if (player['104'].t5) {
+            if (player['104'].t5 || player['104'].t6) {
                 a = [101, 201, 301, 401, 501, 102, 202, 302, 402, 502, 103, 203, 303, 403, 503, 104, 204, 304, 404, 504, 105, 205, 305, 405, 505]
+            }
+            if (player['104'].t5 && player['104'].t6) {
+                a = [101, 201, 301, 401, 501, 601, 102, 202, 302, 402, 502, 602, 103, 203, 303, 403, 503, 603, 104, 204, 304, 404, 504, 604, 105, 205, 305, 405, 505, 605, 106, 206, 306, 406, 506, 606]
             }
         }
         if (fx == 4) {
             a = [104, 204, 304, 404, 103, 203, 303, 403, 102, 202, 302, 402, 101, 201, 301, 401]
-            if (player['104'].t5) {
+            if (player['104'].t5 || player['104'].t6) {
                 a = [105, 205, 305, 405, 505, 104, 204, 304, 404, 504, 103, 203, 303, 403, 503, 102, 202, 302, 402, 502, 101, 201, 301, 401, 501]
             }
+            if (player['104'].t5 && player['104'].t6) {
+                a = [106, 206, 306, 406, 506, 606, 105, 205, 305, 405, 505, 605, 104, 204, 304, 404, 504, 604, 103, 203, 303, 403, 503, 603, 102, 202, 302, 402, 502, 602, 101, 201, 301, 401, 501, 601]
+            }
         }
-        for (j = 0; j < (3 + player['104'].t5); j++)
+        for (j = 0; j < (3 + player['104'].t5 + player['104'].t6); j++)
             for (k = 0; k < a.length; k++) {
                 let i = a[k]
                 let x = Math.floor(i / 100)
                 let y = Math.floor(i % 100)
-                if (fx == 1) x--
-                if (fx == 2) x++
-                if (fx == 3) y--
-                if (fx == 4) y++
-                if (x <= 0 || y <= 0 || x >= (5 + player['104'].t5) || y >= (5 + player['104'].t5)) continue
+                if (fx == 1){
+                    x--
+                    if(player['104'].bl) x--
+                }
+                if (fx == 2){
+                    x++
+                    if(player['104'].bl) x++
+                }
+                if (fx == 3){
+                    y--
+                    if(player['104'].bl) y--
+                }
+                if (fx == 4){
+                    y++
+                    if(player['104'].bl) y++
+                }
+                 if (x <= 0 || y <= 0 || x >= (5+player['104'].t5+player['104'].t6) || y >= (5+player['104'].t5+player['104'].t6)) continue
                 let z = x * 100 + y
                 t = player['104'].grid[z]
                 t1 = player['104'].grid[i]
                 if (t.eq(0) && t1.eq(0)) {
+                    player['104'].canmove = true
                     continue
                 }
                 if (t.neq(0)) {
@@ -213,40 +262,65 @@ addLayer("104", {
         let a = []
         if (fx == 1) {
             a = [101, 102, 103, 104, 201, 202, 203, 204, 301, 302, 303, 304, 401, 402, 403, 404]
-            if (player['104'].t5) {
+            if (player['104'].t5 || player['104'].t6) {
                 a = [101, 102, 103, 104, 105, 201, 202, 203, 204, 205, 301, 302, 303, 304, 305, 401, 402, 403, 404, 405, 501, 502, 503, 504, 505]
+            }
+            if (player['104'].t5 && player['104'].t6) {
+                a = [101, 102, 103, 104, 105, 106, 201, 202, 203, 204, 205, 206, 301, 302, 303, 304, 305, 306, 401, 402, 403, 404, 405, 406, 501, 502, 503, 504, 505, 506, 601, 602, 603, 604, 605, 606]
             }
         }
         if (fx == 2) {
             a = [401, 402, 403, 404, 301, 302, 303, 304, 201, 202, 203, 204, 101, 102, 103, 104]
-            if (player['104'].t5) {
+            if (player['104'].t5 || player['104'].t6) {
                 a = [505, 504, 503, 502, 501, 401, 402, 403, 404, 405, 301, 302, 303, 304, 305, 201, 202, 203, 204, 205, 101, 102, 103, 104, 105]
+            }
+            if (player['104'].t5 && player['104'].t6) {
+                a = [601, 602, 603, 604, 605, 606, 501, 502, 503, 504, 505, 506, 401, 402, 403, 404, 405, 406, 301, 302, 303, 304, 305, 306, 201, 202, 203, 204, 205, 206, 101, 102, 103, 104, 105, 106]
             }
         }
         if (fx == 3) {
             a = [101, 201, 301, 401, 102, 202, 302, 402, 103, 203, 303, 403, 104, 204, 304, 404]
-            if (player['104'].t5) {
+            if (player['104'].t5 || player['104'].t6) {
                 a = [101, 201, 301, 401, 501, 102, 202, 302, 402, 502, 103, 203, 303, 403, 503, 104, 204, 304, 404, 504, 105, 205, 305, 405, 505]
+            }
+            if (player['104'].t5 && player['104'].t6) {
+                a = [101, 201, 301, 401, 501, 601, 102, 202, 302, 402, 502, 602, 103, 203, 303, 403, 503, 603, 104, 204, 304, 404, 504, 604, 105, 205, 305, 405, 505, 605, 106, 206, 306, 406, 506, 606]
             }
         }
         if (fx == 4) {
             a = [104, 204, 304, 404, 103, 203, 303, 403, 102, 202, 302, 402, 101, 201, 301, 401]
-            if (player['104'].t5) {
+            if (player['104'].t5 || player['104'].t6) {
                 a = [105, 205, 305, 405, 505, 104, 204, 304, 404, 504, 103, 203, 303, 403, 503, 102, 202, 302, 402, 502, 101, 201, 301, 401, 501]
+            }
+            if (player['104'].t5 && player['104'].t6) {
+                a = [106, 206, 306, 406, 506, 606, 105, 205, 305, 405, 505, 605, 104, 204, 304, 404, 504, 604, 103, 203, 303, 403, 503, 603, 102, 202, 302, 402, 502, 602, 101, 201, 301, 401, 501, 601]
             }
         }
         for (k = 0; k < a.length; k++) {
             let i = a[k]
             let x = Math.floor(i / 100)
             let y = Math.floor(i % 100)
-            if (fx == 1) x--
-            if (fx == 2) x++
-            if (fx == 3) y--
-            if (fx == 4) y++
-            if (x <= 0 || y <= 0 || x >= (5 + player['104'].t5) || y >= (5 + player['104'].t5)) continue
+            if (fx == 1){
+                x--
+                if(player['104'].bl) x--
+            }
+            if (fx == 2){
+                x++
+                if(player['104'].bl) x++
+            }
+            if (fx == 3){
+                y--
+                if(player['104'].bl) y--
+            }
+            if (fx == 4){
+                y++
+                if(player['104'].bl) y++
+            }
+             if (x <= 0 || y <= 0 || x >= (5+player['104'].t5+player['104'].t6) || y >= (5+player['104'].t5+player['104'].t6)) continue
             let z = x * 100 + y
             t = player['104'].grid[z]
             t1 = player['104'].grid[i]
+            if(t1.eq(-9) || t.eq(-9)) continue
             if (t.eq(-7) && t1.eq(-7)) {
                 player['104'].canmove = true
                 for (j = 1; j <= 4; j++) {
@@ -289,13 +363,15 @@ addLayer("104", {
     },
     udClear() {
         if (!player['104'].ud) return
-        player['104'].losetrig = false
         minn = _D("1e310")
         for (i in player['104'].grid) {
-            if (player['104'].grid[i].gte(0)) minn = minn.min(player['104'].grid[i]).max(0)
+            if ((Math.floor(i/100) > (4+player['104'].t5+player['104'].t6)) || ((i%100) > (4+player['104'].t5+player['104'].t6))) continue
+            console.log(i)
+            if (player['104'].grid[i].gt(0)) minn = minn.min(player['104'].grid[i]).max(0)
         }
         for (i in player['104'].grid) {
-            if (player['104'].grid[i].eq(minn) || player['104'].grid[i].lt(0)) {
+            console.log(i)
+            if (player['104'].grid[i].eq(minn) || (player['104'].grid[i].lt(0) && player['104'].grid[i].neq(-9))) {
                 player['104'].grid[i] = _D0
                 player['104'].cnt--
             }
@@ -355,6 +431,10 @@ addLayer("104", {
                 player['104'].cnt = 0
                 layers['104'].numGen()
                 layers['104'].numGen()
+                if(player['104'].ob5){
+                    if(player['104'].grid[101].eq(0)) player['104'].cnt++
+                    player['104'].grid[101] = _D(-9)
+                }
             },
             unlocked() { return true },
             canClick() { return player['104'].losetrig },
@@ -365,7 +445,6 @@ addLayer("104", {
             display: "死局时自动消除所有最小方块和障碍块",
             onClick() {
                 player['104'].ud = !player['104'].ud
-                layers['104'].udClear()
             },
             style: {
                 height: "100px", width: "100px", minHeight: "100px", marginTop: "15px", border: "4px solid", borderColor: "#00c2e0ff", "background-color"() {
@@ -523,15 +602,87 @@ addLayer("104", {
             unlocked() { return player['104'].trig },
             canClick() { return player['104'].losetrig },
         },
+        51: {
+            title() { return `OB5` },
+            display: "游戏开始时获得一个无法消除的NaN块",
+            onClick() {
+                player['104'].ob5 = !player['104'].ob5
+            },
+            style: {
+                height: "100px", width: "100px", minHeight: "100px", marginTop: "15px", border: "4px solid", borderColor: "#e00000ff", "background-color"() {
+                    if (player['104'].ob5) return "#e00000ff"
+                    return "#e0000033"
+                }, "color"() {
+                    if (player['104'].ob5) return "rgb(0,0,0)"
+                    return "#e00000ff"
+                }
+            },
+            unlocked() { return player['104'].trig },
+            canClick() { return player['104'].losetrig },
+        },
+        52: {
+            title() { return `BL` },
+            display: "合并和移动规则改为跨1格",
+            onClick() {
+                player['104'].bl = !player['104'].bl
+            },
+            style: {
+                height: "100px", width: "100px", minHeight: "100px", marginTop: "15px", border: "4px solid", borderColor: "#00c2e0ff", "background-color"() {
+                    if (player['104'].bl) return "#00c2e0"
+                    return "#00c2e033"
+                }, "color"() {
+                    if (player['104'].bl) return "rgb(0,0,0)"
+                    return "#00c2e0"
+                }
+            },
+            unlocked() { return player['104'].trig },
+            canClick() { return player['104'].trig },
+        },
+        53: {
+            title() { return `T+` },
+            display: "棋盘规格+1",
+            onClick() {
+                player['104'].t6 = !player['104'].t6
+            },
+            style: {
+                height: "100px", width: "100px", minHeight: "100px", marginTop: "15px", border: "4px solid", borderColor: "#e00000ff", "background-color"() {
+                    if (player['104'].t6) return "#e00000ff"
+                    return "#e0000033"
+                }, "color"() {
+                    if (player['104'].t6) return "rgb(0,0,0)"
+                    return "#e00000ff"
+                }
+            },
+            unlocked() { return player['104'].trig },
+            canClick() { return player['104'].losetrig },
+        },
+        54: {
+            title() { return `CH` },
+            display: "每次移动有4%概率打乱盘面",
+            onClick() {
+                player['104'].ch = !player['104'].ch
+            },
+            style: {
+                height: "100px", width: "100px", minHeight: "100px", marginTop: "15px", border: "4px solid", borderColor: "#00c2e0ff", "background-color"() {
+                    if (player['104'].ch) return "#00c2e0"
+                    return "#00c2e033"
+                }, "color"() {
+                    if (player['104'].ch) return "rgb(0,0,0)"
+                    return "#00c2e0"
+                }
+            },
+            unlocked() { return player['104'].trig },
+            canClick() { return player['104'].trig },
+        },
     },
     grid: {
-        rows: 5,
-        cols: 5,
+        rows: 6,
+        cols: 6,
         getStartData(id) {
             return _D0
         },
         getUnlocked(id) {
-            if ((id % 100 == 5 || (Math.floor(id / 100) == 5)) && (!player['104'].t5)) return false
+            if((Math.floor(id/100) >= (5+player['104'].t5+player['104'].t6)) || ((id%100) >= (5+player['104'].t5+player['104'].t6))) return false
             return true
         },
         getCanClick(data, id) {
@@ -542,6 +693,7 @@ addLayer("104", {
         },
         getDisplay(data, id) {
             if (player['104'].ob3) return "?"
+            if (data.eq(-9)) return "NaN"
             if (data.eq(-8)) return "D"
             if (data.eq(-7)) return "M"
             if (data.eq(-6)) return "/2"
@@ -552,6 +704,7 @@ addLayer("104", {
             let style
             if (player['104'].ob3 && data.neq(0)) style = { backgroundColor: "#ffffff" }
             else if (data.eq(0)) style = { backgroundColor: "black", border: "2px solid", borderColor: "#ffffff" }
+            else if (data.eq(-9)) style = { backgroundColor: "#ee0000" }
             else if (data.eq(-8)) style = { backgroundColor: "#005e35" }
             else if (data.eq(-7)) style = { backgroundColor: "#ff7b00" }
             else if (data.eq(-6)) style = { backgroundColor: "#ffffff" }
