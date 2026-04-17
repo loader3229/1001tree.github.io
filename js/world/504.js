33b619ec2f15ef4510987718a4448a756f709c3c
banana3864 <owen_0624@outlook.com>
2026-04-18 01:30:19 +0800

    504 test
    504 levels completed, require testing and evaluation on general difficulty

--- js/world/504.js
+++ js/world/504.js
@@ -33,6 +33,8 @@ addLayer("504", {
         ["blank","60px"],
         ["row",[
             ["column",[
+                ["display-text","下方显示当前选择的拼图块<br>放置拼图的拼图板在右边"],
+                "blank",
                 ["clickables","4"],
                 "blank",
                 ["layer-proxy",["402",["grid"]]],
@@ -41,8 +43,8 @@ addLayer("504", {
             ]],
             ["blank",["50px","150px"]],
             "grid"    
-            
-        ]]
+        ]],
+        ["blank","100px"]
     ],
     clickables: {
         11: {
@@ -89,15 +91,23 @@ addLayer("504", {
             style() {return {"width":"190px","border":"4px solid","border-radius":"2px","background-color":"#F1C375","border-color":"#D89C58"}},
             canClick() {return this.unlocked()},
             onClick() {
+                if(player[504].level==26)return
                 let c=[]
+                let d=[]
                 Object.entries(player[504].grid).forEach(([id,data])=>{
                     if(data504[player[504].level][4].includes(Number(id))||Math.floor(Number(id)/100)>data504[player[504].level][1]||Number(id)%100>data504[player[504].level][2])return
-                    c.push([Number(id),1])
+                    let w=data504[player[504].level][6].find(x=>{return x[0]==Number(id)})
+                    if (w==undefined)w=[-1,1]
+                    c.push([Number(id),w[1]])
                 })
                 let t=[]
-                player[504].tileList.forEach(a=>{t=t.concat(a)})
+                player[504].tileList.forEach(a=>{
+                    t=t.concat(a)
+                    d.push(a[0])
+                })
                 let v=true
                 c.forEach(([id,x])=>{t.forEach(v=>{if(v==id)x--});if(x!=0)v=false})
+                data504[player[504].level][5].forEach(id=>{if(!d.includes(id))v=false})
                 if(v){player[504].maxLevel=Math.max(player[504].maxLevel,++player[504].level);player[504].tileList=[]}
             }
         },
@@ -143,15 +153,31 @@ addLayer("504", {
         getStartData(id) {return 0},
         getUnlocked(id) {return true},
         getCanClick(data, id) {return !data504[player[504].level][4].includes(id)},
+        getTitle(data, id) {
+            let c=-1
+            let s=player[504].tileList.reduce((x,y)=>x+y.includes(id),0)
+            data504[player[504].level][6].forEach(l=>{if(l[0]==id)c=l[1]})
+            if(c==-1&&s<=1)return ""
+            return c==-1?s:`<h2>${s} / ${c}</h2>`
+        },
         getStyle(data, id) {
             let color=data504[player[504].level][4].includes(id)?[20,22,24]:[150,144,156]
-            for (i in player[504].tileList){if(player[504].tileList[i].includes(id))color=HEXtoRGB(data504["color"][i])}
+            let bType=data504[player[504].level][4].includes(id)?"5px solid":"4px outset"
+            let bColor=data504[player[504].level][4].includes(id)?[18,32,32]:[225,240,255]
+            let flag=false
+            for (i in player[504].tileList){
+                if(player[504].tileList[i].includes(id)){
+                    color=HEXtoRGB(data504["color"][i%16])
+                    if(player[504].tileList[i][0]==id)flag=true
+                }
+            }
+            if (data504[player[504].level][5].includes(id)){bType="5px solid";bColor=flag?[63,240,31]:[255,31,31]}
             if (player[504].hoveringPreview.map(([x,y])=>100*x+y+player[504].hoverID).includes(id)) color=color.map(v=>Math.floor(Math.min(v*(1.35+0.4*Math.sin(player[504].resetTime*5)),255)))
-            return {"height":"72px","width":"72px","border":`4px ${data504[player[504].level][4].includes(id)?"solid #112222":"outset #DDEEFF"}`,"background-color":`${RGBtoHEX(color)}`,"border-radius":"0px","transition-duration":"0.3s","transform":"scale(1,1)"}
+            return {"height":"72px","width":"72px","border":`${bType} ${RGBtoHEX(bColor)}`,"background-color":`${RGBtoHEX(color)}`,"border-radius":"0px","transition-duration":"0.3s","transform":"scale(1,1)"}
         },
         onClick(data, id) {
             let valid=true
-            let prep=tiles504[data504[player[504].level][3][player[504].current]][4].map(([x,y])=>{let n=orthogonalRotation([x,y],player[504].rotation);let p=n[0]*100+n[1]+id;if(data504[player[504].level][4].includes(p)){valid=false;console.log(p)};return p})
+            let prep=tiles504[data504[player[504].level][3][player[504].current]][4].map(([x,y])=>{let n=orthogonalRotation([x,y],player[504].rotation);let p=n[0]*100+n[1]+id;if(data504[player[504].level][4].includes(p)){valid=false};return p})
             if(valid)player[504].tileList.push(prep)
         }
     },
