4b7068a59253488931ee6c297770d75b9b494955
QHMY114514 <1550187725@qq.com>
2026-05-01 19:03:00 +0800

    更改版本号

--- js/mod.js
+++ js/mod.js
@@ -19,11 +19,13 @@ let modInfo = {
 // 在num和name中设置版本号
 let VERSION = {
 	num: 0.72,
-	name: ""
+	name: "发布版v1"
 }
 
 let changelog = `
 	<h1>更新日志:</h1><br><br>
+	<h3>v1|0.72 | 2025/5/1</h3><br>
+	发布版<br><br>
 	<h3>v0.76? | 2025/3/15</h3><br>
 	更新了19个游戏<br><br>
 	<h3>v0.68 | 2025/1/16</h3><br>
